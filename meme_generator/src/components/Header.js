4de71e3bf14599776058f22ea3fb026574f004cd
Collin Travers <collintravers1@gmail.com>
2023-02-28 16:07:31 -0500

    App creation and header components/styling first completion

---
+++ meme_generator/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Header.css';
+
+function Header() {
+  return (
+    <div>
+      <header className="header">
+        <img className="header--image" src="./images/troll-face.png"/>
+        <h2 className="header--title">Meme Generator</h2>
+        <h4 className="header--project">React Course - Project 3</h4>
+      </header>
+    </div>
+  )
+}
+
+export default Header;
